63286b13859c00b6fb1ef00feb1ced617d24a4a7
sbiswas2209 <sbiswas2209@outlook.com>
2021-11-10 19:00:19 +0530

    ✨ Add JSONPlaceholder API Integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import PersonTile from "./components/PersonTile";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    async function getPeople() {
+      const result = await fetch(`https://jsonplaceholder.typicode.com/users`, {
+        method: "GET",
+      });
+      const data = await result.json();
+      setPeople(data);
+    }
+    getPeople()
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {people.length === 0 ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          {people.map((e) => (
+            <PersonTile name={e.name} phone={e.phone} id={e.id} key={e.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
